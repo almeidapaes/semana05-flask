14bcc4ae61f440e496d1489e5029bea92470ae15
almeidapaes <almeida.paes@aluno.ifsp.edu.br>
2026-08-17 22:35:38 -0300

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave-secreta-semana05'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
